80e6f70f21095665b3e0327c25873e433a806806
Marie-Chantale Clay <marie-anne.clay@lgm.fr>
2023-08-02 17:46:51 +0200

    Tente une autre approche plus porter sur la logique/algorithme du quotient et du reste, refacto du code en ce sens jusqu'à présent, aulieu de partir du plus petit nombre enlevé le plus grand nombre et ainsi de suite, les tests passent sauf XLV, à revoir...

--- lacombedulionvert/src/main/java/lacombedulionvert/ConvertionBetweenRomainAndArabicNumber.java
+++ lacombedulionvert/src/main/java/lacombedulionvert/ConvertionBetweenRomainAndArabicNumber.java
@@ -1,87 +1,50 @@
 package lacombedulionvert;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class ConvertionBetweenRomainAndArabicNumber {
+	Map<Integer, String> RomainConstant = new LinkedHashMap<Integer, String>();
+	Map<Integer, String> RomainException = new LinkedHashMap<Integer, String>();
 	StringBuilder builder;
 	private static int V = 5;
 	private static int X = 10;
 	
+	public ConvertionBetweenRomainAndArabicNumber(){
+		this.RomainConstant.put(50, "L");
+		this.RomainConstant.put(10, "X");
+		this.RomainConstant.put(5, "V");
+		this.RomainConstant.put(1, "I");
+		
+		this.RomainException.put(40, "XL");
+		this.RomainException.put(9, "IX");
+		this.RomainException.put(4, "IV");
+	}
+	
 	public String ConvertFromArabicToRomain(int arabicNumber) {
 		this.builder = new StringBuilder();
+		int reste = arabicNumber;
+		int quotient = 0;
 
-		if(50 <= arabicNumber) {
-			builder.append("L");
-			
-			if(50 < arabicNumber) {
-				this.ConvertWithL(arabicNumber - 50);
+		for (Map.Entry<Integer, String> entry : this.RomainConstant.entrySet()) {
+			quotient = reste / entry.getKey();
+			for(int i = 0; i < quotient; i++) {
+				this.builder.append(entry.getValue());
 			}
-		}	
-		else {
-			this.ConvertWithL(arabicNumber);
+			
+			reste = reste % entry.getKey();
+			
+			if(this.RomainException.containsKey(reste)) {
+				this.builder.append(this.RomainException.get(reste));
+				reste = 0;
+			}
+			
+			if(reste == 0) {
+				break;
+			}
 		}	
 		
 		return builder.toString();
 	}
 
-	private void ConvertWithL(int arabicNumber) {
-		if(40 <= arabicNumber) {
-			builder.append("XL");
-			
-			if(40 < arabicNumber) {
-				this.ConvertWithXAndV(arabicNumber - 40);
-			}
-		}
-		else if(arabicNumber > 13) {
-			int quotient = arabicNumber / ConvertionBetweenRomainAndArabicNumber.X;
-			
-			for(int i = 0; i < quotient; i++) {
-				this.builder.append("X");
-			}
-			
-			int resteX = arabicNumber % ConvertionBetweenRomainAndArabicNumber.X;
-			this.ConvertWithXAndV(resteX);
-		}
-		else {
-			this.ConvertWithXAndV(arabicNumber);
-		}
-	}
-	
-	/**
-	 * Les chiffres de 4 à 13 (avec ConvertWithI de 1 à 3 et de 11 à 13)
-	 * @param arabicNumber le nombre en écriture arabe
-	 */
-	private void ConvertWithXAndV(int arabicNumber) {
-		if(0 < arabicNumber && arabicNumber < 4) {
-			this.ConvertWithI(arabicNumber);
-		}
-		else if(arabicNumber == 4) {
-			builder.append("IV");
-		}
-		else if(ConvertionBetweenRomainAndArabicNumber.V <= arabicNumber && arabicNumber < 9) {
-			builder.append("V");
-			
-			if(ConvertionBetweenRomainAndArabicNumber.V < arabicNumber) {
-				this.ConvertWithI(arabicNumber - ConvertionBetweenRomainAndArabicNumber.V);
-			}
-		}
-		else if(arabicNumber == 9) {
-			builder.append("IX");
-		}			
-		else if(ConvertionBetweenRomainAndArabicNumber.X <= arabicNumber) {
-			builder.append("X");
-			
-			if(ConvertionBetweenRomainAndArabicNumber.X < arabicNumber) {
-				this.ConvertWithI(arabicNumber - ConvertionBetweenRomainAndArabicNumber.X);
-			}
-		}
-	}
-
-	/**
-	 * Les chiffres de 1 à 3 (qui s'écrivent uniquement avec des I)
-	 * @param arabicNumber le nombre en écriture arabe
-	 */
-	private void ConvertWithI(int arabicNumber) {
-		for(int i = 0; i < arabicNumber; i++) {
-			builder.append("I");
-		}
-	}
 }
