8567bb3736ada7b9e24af787e289ce8d61a861ff
Marie-Chantale Clay <marie-anne.clay@lgm.fr>
2023-08-03 11:29:37 +0200

    Validation du cas XLV en passant 40 de exception à la liste constant

--- lacombedulionvert/src/main/java/lacombedulionvert/ConvertionBetweenRomainAndArabicNumber.java
+++ lacombedulionvert/src/main/java/lacombedulionvert/ConvertionBetweenRomainAndArabicNumber.java
@@ -3,39 +3,39 @@ package lacombedulionvert;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+/**
+ * Convertir les nombres de romain à arabe et inversement de 1 à 3 999
+ */
 public class ConvertionBetweenRomainAndArabicNumber {
 	Map<Integer, String> RomainConstant = new LinkedHashMap<Integer, String>();
 	Map<Integer, String> RomainException = new LinkedHashMap<Integer, String>();
-	StringBuilder builder;
-	private static int V = 5;
-	private static int X = 10;
 	
 	public ConvertionBetweenRomainAndArabicNumber(){
 		this.RomainConstant.put(50, "L");
+		this.RomainConstant.put(40, "XL");
 		this.RomainConstant.put(10, "X");
 		this.RomainConstant.put(5, "V");
 		this.RomainConstant.put(1, "I");
 		
-		this.RomainException.put(40, "XL");
 		this.RomainException.put(9, "IX");
 		this.RomainException.put(4, "IV");
 	}
 	
 	public String ConvertFromArabicToRomain(int arabicNumber) {
-		this.builder = new StringBuilder();
+		StringBuilder builder = new StringBuilder();
 		int reste = arabicNumber;
 		int quotient = 0;
 
 		for (Map.Entry<Integer, String> entry : this.RomainConstant.entrySet()) {
 			quotient = reste / entry.getKey();
 			for(int i = 0; i < quotient; i++) {
-				this.builder.append(entry.getValue());
+				builder.append(entry.getValue());
 			}
 			
 			reste = reste % entry.getKey();
 			
 			if(this.RomainException.containsKey(reste)) {
-				this.builder.append(this.RomainException.get(reste));
+				builder.append(this.RomainException.get(reste));
 				reste = 0;
 			}
 			
